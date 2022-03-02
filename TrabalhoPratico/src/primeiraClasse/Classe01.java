c2febe097e9d98859c61bccd97b166340f1732e7
jfnas <jfnas@DESKTOP-E14BUGV>
2022-03-02 16:13:54 -0300

    Commit de teste

---
+++ TrabalhoPratico/src/primeiraClasse/Classe01.java
@@ -0,0 +1,10 @@
+package primeiraClasse;
+
+public class Classe01 {
+
+	public static void main(String[] args) {
+		System.out.print("Olá mundo\n");
+		System.out.print("Meu primeiro programa em java!!");
+	}
+
+}
